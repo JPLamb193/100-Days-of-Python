af25242683c936e7e6ab9840ce678841859ec2dd
JPLamb193 <jlamb119933@gmail.com>
2023-05-02 15:59:34 -0500

    Finished headsOrTails exercise

---
+++ Day 4/headsOrTails.py
@@ -0,0 +1,10 @@
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+	 
+#Write the rest of your code below this line 👇
+import random
+roll = random.randint(0,1)
+if roll == 0:
+    print("Heads")
+if roll == 1:
+    print("Tails")
